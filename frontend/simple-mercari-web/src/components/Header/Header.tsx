a3abdb7590f6ae4d54a54c50686225dc85255782
Tomomi Kanai <tomomin001104@gmail.com>
2023-05-29 05:45:19 +0900

    logo, balance

--- frontend/simple-mercari-web/src/components/Header/Header.tsx
+++ frontend/simple-mercari-web/src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import React, { useState, MouseEvent, ChangeEvent } from 'react'
 import { useCookies } from "react-cookie";
 import { Link } from 'react-router-dom';
 import { useAppContext } from '../../contexts/AppContext';
+import { useParams, useNavigate } from "react-router-dom";
 
 const SearchIcon = () => <svg className="button-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
   <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
@@ -12,6 +13,7 @@ export const Header: React.FC = () => {
   const app = useAppContext();
   const [cookies] = useCookies(["name", "userID", "token"]);
   const [name, setName] = useState("")
+  const navigate = useNavigate();
 
   const onLogout = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
@@ -22,7 +24,12 @@ export const Header: React.FC = () => {
   return <header className='w-full px-4 flex flex-row justify-between items-center text-theme-100 bg-theme-800'style={{  top: '0' }}>
     <p>
     <b className='text-white'>
-        <img src="/logo.png" alt="Simple Mercari" style={{ height: "120px", width: "auto" ,marginTop: "50px"}} />
+        <img 
+          src="/logo.png"
+          alt="Simple Mercari"
+          style={{ height: "120px", width: "auto" ,marginTop: "-10px",position: "absolute", zIndex: 999}} 
+          onClick={() => navigate(`/`)}
+        />
       </b>
     </p>
     <div className='flex items-center'>
